de25ebe429491c1d860532ca003ed026517bc13f
mtohernandez <m4teoh3rnandez@outlook.com>
2023-06-14 18:43:53 -0500

    Fix default route

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,26 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+const routes = [
+  {
+    path: "/",
+    element: <App />,
+  },
+];
+
+const router = createBrowserRouter(
+  routes,
+  {
+    basename: "/food-city/",
+  }
+);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
